ac11cd37fa33b2e510226c6775c66584d7f01fee
Bianca Fu <0509biancafu@gmail.com>
2023-04-02 20:57:42 -0600

    implemented load in images and gravity

--- main.py
+++ main.py
@@ -16,19 +16,49 @@ BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.jpg
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites] #true: flip x direction, false: don't flip y direction
+
+def load_sprite_sheets(dir1, dir2, width, height, direction = False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha() #transparent background
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCAPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+        
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+    return all_sprites
+
+
+
 class Player(pygame.sprite.Sprite): #inheriting from sprite for pixel accurate collision (use their methods)
     COLOR = (255, 0, 0)
-    GRAVITY = 1 #33:33
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
     
     def __init__(self, x, y, width, height) -> None:
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
         self.y_vel = 0
         self.mask = None
-
         #for showing animation later
         self.direction = "left" 
         self.animation_count = 0
+        #for gravity
+        self.fall_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -46,12 +76,24 @@ class Player(pygame.sprite.Sprite): #inheriting from sprite for pixel accurate c
             self.direction = "right"
             self.animation_count = 0
     
-    def loop(self, fps):
+    def loop(self, fps): #looping for each frame
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
-    def draw(self, window):
-        pygame.draw.rect(window, self.COLOR, self.rect)
+        self.fall_count += 1
 
+    def update_sprite(self):
+        sprite_sheet = "idle" #default sprite
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprite = self.SPRITES[sprite_sheet_name]
+        
+
+    def draw(self, window):
+        self.sprite = self.SPRITES["idle_" + self.direction]
+        window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 def draw(window, player):
